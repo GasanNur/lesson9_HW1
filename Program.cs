c4c23bd2ceb26b15c5b1506f0d61a57d3ee1583d
Gasan <1793959@mail.rum>
2023-03-16 00:33:53 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+
+Console.Write("Введите первое число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int max = Convert.ToInt32(Console.ReadLine());;
+NaturalToLow(min, max);
+
+
+void NaturalToLow(int min, int max)
+{
+    if (min > max) return;
+    if(min % 2 == 0) Console.Write($"{min}\t");    
+    {
+        NaturalToLow(min + 1, max );
+        System.Console.WriteLine();
+    }
+}
